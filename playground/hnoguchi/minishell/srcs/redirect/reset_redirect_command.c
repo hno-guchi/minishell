0ab10ac19910365a17c1c9ec2d642fcc3218d860
hnoguchi <>
2023-03-23 12:05:29 +0900

    Delete if (token->kind == TK_REDIRECTION)

--- playground/hnoguchi/minishell/srcs/redirect/reset_redirect_command.c
+++ playground/hnoguchi/minishell/srcs/redirect/reset_redirect_command.c
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 11:58:37 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/03/22 12:03:38 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/03/23 12:04:51 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,6 @@ static void	close_redirect_file(int *target_fd, t_token *args)
 		return ;
 	}
 	close_redirect_file(target_fd, args->next);
-	if (args->kind == TK_REDIRECTION)
-	{
-		return ;
-	}
 	reset_close(target_fd);
 	if (dup2(args->stashed_file_fd, *target_fd) < 0)
 	{
